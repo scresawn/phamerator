23e98becf6d5442a826e1f11f000953b8415cf45
Kaleigh Jaeger <kjaeger123@gmail.com>
2018-09-21 22:24:25 -0600

    Added working modal
    Used my current version of the code including features I've been working on, so just ignore everything else.  I added a link named "Statistics" above the title on the Home page.  When clicked, it redirects to a page where the created modal automatically opens.  I also added a button on this page that is supposed to make the modal reappear, but haven't gotten it to work yet.

--- phamerator/client/js/domains.js
+++
@@ -1,8 +0,0 @@
-Template.domains.onRendered(function () {
-  $(document).ready(function () {
-
-  });
-  $('.collapsible').collapsible({
-    accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-  });
-});
