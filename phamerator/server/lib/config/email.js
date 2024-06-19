62a5c3f77cd0d59a5211802967de2b850a397d05
Steven Cresawn <scresawn@gmail.com>
2024-06-19 18:03:10 -0400

    get all formal with my name

--- phamerator/server/lib/config/email.js
+++ phamerator/server/lib/config/email.js
@@ -1,15 +1,15 @@
 Accounts.emailTemplates.siteName = "mail.phamerator.org";
 Accounts.emailTemplates.from = "Phamerator <info@mail.phamerator.org>";
 Accounts.emailTemplates.resetPassword.subject = function (user) {
- return "Phamerator password reset";
+  return "Phamerator password reset";
 };
 
- Accounts.emailTemplates.resetPassword.text = function (user, url) {
- var signature = "Steve Cresawn";
+Accounts.emailTemplates.resetPassword.text = function (user, url) {
+  var signature = "Steven Cresawn";
 
- return "Please click the following link to set your new password:\n" +
- url + "\n\n" +
- "Thanks for using Phamerator!\n\n" +
- "Cheers,\n" +
- signature;
- };
+  return "Please click the following link to set your new password:\n" +
+    url + "\n\n" +
+    "Thanks for using Phamerator!\n\n" +
+    "Cheers,\n" +
+    signature;
+};
